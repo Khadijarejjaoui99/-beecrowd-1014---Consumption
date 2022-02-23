fcf71d89735bac4eac1533ce8ffd3163f1ff5629
FIRST_NAME LAST_NAME <khadijarejjaoui41@gmail.com>
2022-02-23 21:29:17 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,15 @@
+// Solution of 1014 - Consumption in beecrowd
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main ()
+{
+  int X;
+  float Y;
+  cin >> X >> Y;
+  cout << fixed << setprecision (3) << X / Y << " km/l" << endl;
+    system("pause");
+    return 0;
+}
